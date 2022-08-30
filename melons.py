5a2e2eabbfcf35fda78eff380f21db6f6b5cde52
Chloe Macias <chloeamacias@gmail.com>
2022-08-30 15:29:18 -0400

    Initial commit.

---
+++ melons.py
@@ -0,0 +1,49 @@
+"""Melon and Squash classes."""
+
+import robots
+
+
+class AbstractMelon:
+    """Melon."""
+
+    def __init__(self, melon_type):
+        """Initialize melon.
+
+        melon_type: type of melon being built.
+        """
+
+        self.melon_type = melon_type
+        self.weight = 0.0
+        self.color = None
+        self.stickers = []
+
+    def prep(self):
+        """Prepare the melon."""
+
+        robots.cleanerbot.clean(self)
+        robots.stickerbot.apply_logo(self)
+
+    def __str__(self):
+        """Print out information about melon."""
+
+        if self.weight <= 0:
+            return self.melon_type
+        else:
+            return f"{self.color} {self.weight:.2f} lbs {self.melon_type}"
+
+
+class Squash(AbstractMelon):
+    """Winter squash."""
+
+    def __init__(melon_type):
+        super().__init__(melon_type)
+        super().prep()
+        super().__str__()
+
+    def paint(self):
+        """Paints winter squash green."""
+        robots.painterbot.paint(self)
+        self.color = "Green"
+
+
+    
